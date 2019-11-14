71b934b3be5d8f9731cbb7798bdc4d86722d90c8
fudma135 <maja.fudala@lo1.sandomierz.pl>
2019-11-14 12:34:14 +0100

    algorytmy iteracyjne

--- python/petla.py
+++ python/petla.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def main(args):
+def sumuj_parzyste(start, stop):
+    
     suma = 0
-    for i in range(3):
-        liczba = int(input("podaj liczbę: "))
-        suma = suma + liczba
+    for i in range(start,stop+1,2):
+        #liczba = int(input("podaj liczbę: "))
+        suma = suma + i
     print(suma)
-        
+
+
+def drukuj_nieparzyste(start, stop+1):
+    #if start % 2 == 0:
+        #start = start + 1
+    if i in range(start,stop+1,2):
+        if i % 2 ==1:
+            print(i)
+
+
+def main(args):
+    start=int(input("podaj początek zakresu:"))
+    stop=int(input("podaj koniec zakresu:"))
+    #sumuj_parzyste(start, stop)
+    drukuj_nieparzyste(start,stop)
+    
     return 0
 
 if __name__ == '__main__':
