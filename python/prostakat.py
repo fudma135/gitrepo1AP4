ffd0663d0295f44330373437bb203c365754f1e3
fudma135 <maja.fudala@lo1.sandomierz.pl>
2019-12-18 11:40:29 +0100

    zmiany

---
+++ python/prostakat.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  prostakat.py
+#  
+#  Copyright 2019  <>
+#
+#  
+
+
+def main(args):
+    znak=int(input("podaj znak:")
+    w=int(input("podaj wysokość:"))
+    s=int(input("podaj szerokość:"))
+    
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
---
+++ python/zliczanie.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  zadanie.py
+
+def main(args):
+    a=int(input("podaj liczbę a:"))
+
+    
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
