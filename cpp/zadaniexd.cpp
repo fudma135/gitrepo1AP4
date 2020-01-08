c83b4e64449352ab0cbe0c27299e79d4e46e5140
fudma135 <maja.fudala@lo1.sandomierz.pl>
2020-01-08 11:35:12 +0100

    zmiany

---
+++ cpp/zadaniexd.cpp
@@ -0,0 +1,35 @@
+/*
+ * zadaniexd.cpp
+ * 
+ * Copyright 2020  <>
+ * 
+
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+}
+
+int main(int argc, char **argv)
+{
+	float ocena, liczba_ocen,suma;
+suma = 0
+cout<< "podaj liczbe ocen:";
+cin>>liczba_ocen;
+while (liczba_ocen<1) {
+    cout<< "podaj liczbę ocen:";
+    cin>>liczba_ocen;
+}
+for(5)
+cout<<"podaj ocene:";
+cin>>ocena;
+while (ocena<1 or ocena >6){
+    cout<<"podaj prawidłową ocenę:";
+    cin>>ocena;
+    suma=suma+ocena;
+	return 0;
+}
+
