91e1a390f1e747cb343b465260f66295b3dfb661
fudma135 <maja.fudala@lo1.sandomierz.pl>
2020-01-23 12:36:40 +0100

    kalkulator v2

--- python/kalkulator.py
+++ python/kalkulator.py
@@ -5,14 +5,43 @@
 #  
 
 wynik = 0
-def
 
+def pobierzLiczbe():
+    licz1=int(input("podaj liczbe:"))
+    return licz1
+    
+def dodaj(licz1,licz2):
+    global wynik
+    wynik=licz1+licz2
+    return suma
+    
+def odejmij(licz1,licz2):
+    wynik=licz1-licz2
+    return różnica
+    
+def pomnoz(licz1,licz2):
+    wynik=licz1*licz2
+    
+def podziel(licz1,licz2):
+    wynik=licz1/licz2
+
+def drukujWynik():
+
+    
 def main(args):
     znak=input("podaj znak(+,-,*,/):")
-    licz1=int(input("podaj liczbe1:"))
-    licz2=int(input("podaj liczbe2:"))
-    
-    
+    licz1=pobierzLiczbe()
+    licz2=pobierzLiczbe()
+    if znak== '+':
+        dodaj(licz1,licz2)
+    elif znak== '-':
+        odejmij(licz1,licz2)
+    elif znak== '/':
+        podziel(licz1,licz2)
+    elif znak=='*':
+        pomnoz(licz1,licz2)
+    else:
+        print('błędny znak działania')
     return 0
 
 if __name__ == '__main__':
