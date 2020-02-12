45c57ff978939908614aff69cfecafd51710915d
fudma135 <maja.fudala@lo1.sandomierz.pl>
2020-02-12 11:40:06 +0100

    zmiany

---
+++ cpp/cw1tab.cpp
@@ -0,0 +1,43 @@
+/*
+ * cw1tab.cpp
+ * 
+ * Copyright 2020  <>
+ 
+ */
+
+
+#include <iostream>
+using namespace std;
+void pobierzLiczby(int t[], int n){
+    for(int i=0; i<n; i++) {
+        cout<<"Podaj liczbe:";
+       cin>>t[i];
+}
+}
+
+
+void drukuj(int t[], int n){
+    for(int i=0; i<n; i++){
+        ;
+    }
+    cout<<endl;
+    }
+    float sumujTab(int t1[], int n) {
+    int suma= 0;
+    
+        }
+    
+int main(int argc, char **argv)
+{  int n=5; 
+    
+    int t1[5];
+  int t2[5];
+  pobierzLiczby(t1,n);
+  pobierzLiczby(t2,n);
+  drukuj(t1,n);
+  drukuj(t2,n);
+ int s1=sumujTab(t1,n);
+ int s2=sumujTab(t2,n);
+	return 0;
+}
+
